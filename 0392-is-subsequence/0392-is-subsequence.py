ad1a79342865a747b052821197d8da863985a25a
Pranay <111632387+Git4pranay@users.noreply.github.com>
2023-06-16 05:23:12 +0530

    Time: 39 ms (83.99%), Space: 16.3 MB (52%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -3,7 +3,7 @@ class Solution:
         count = 0;l = len(s)
         for i in range(len(t)):
             if count < l and s[count] == t[i] :
-                count += 1
+                count+= 1
         if count==l:
             return 1
         return 0
