047d428ad1f993e35c10d4fdd651b2f656625b1d
Pranay <111632387+Git4pranay@users.noreply.github.com>
2023-06-16 06:08:28 +0530

    Time: 44 ms (59.01%), Space: 16.3 MB (48.03%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -3,8 +3,7 @@ class Solution:
         j=1;g=0
         for i in range(len(s)-1,-1,-1):
             if s[i]!=' ':
-                j=0
-                g+=1
+                j=0;g+=1
             else:
                 if j==0:
                     break
