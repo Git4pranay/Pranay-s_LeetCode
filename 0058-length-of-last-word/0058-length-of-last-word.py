cd2c3844643587fac3ebafd13d08756aa3d05a58
Pranay <111632387+Git4pranay@users.noreply.github.com>
2023-06-16 06:08:02 +0530

    Time: 43 ms (66.56%), Space: 16.5 MB (13.7%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -2,11 +2,11 @@ class Solution:
     def lengthOfLastWord(self, s):
         j=1;g=0
         for i in range(len(s)-1,-1,-1):
-            if s[i]==' ':
-                if j==0:
-                    break
-            else:
+            if s[i]!=' ':
                 j=0
                 g+=1
-        return (g)
+            else:
+                if j==0:
+                    break
+        return g
         
