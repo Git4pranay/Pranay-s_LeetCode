ed70b0dd786db66a58658c4d966ead3ff1989dfe
Pranay <111632387+Git4pranay@users.noreply.github.com>
2023-06-18 00:18:31 +0530

    Time: 115 ms (64.75%), Space: 16.4 MB (40.33%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,44 +1,21 @@
-
-
 class Solution:
-    
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)  # key = (r /3, c /3)
 
-    def isValidSudoku(self, board):
-        def three(ele,i,j,board):
-            
-            a = i - i%3
-            b = j - j%3
-            c=0
-            for i in range(a,a+3):
-                for j in range(b,b+3):
-                    if ele==board[i][j]:
-                        c+=1
-            if c>1:
-                        return 0
-            return 1
-        
-        def col(ele,j,board):
-            c=0
-            for i in range(9):
-                if board[i][j]==ele:
-                    c=c+1
-            if c>1:
-                return 0
-            return 1
-            
-        def row(ele,i,board):
-            if board[i].count(ele)>1:
-                return 0
-            return 1
-        for i in range(9):
-            for j in range(9):
-                if board[i][j]!='.':
-                   if not(col(board[i][j],j,board)):
-                       return 0
-                   if not(row(board[i][j],i,board)):
-                       return 0
-                   if not(three(board[i][j],i,j,board)):
-                       return 0
-        return 1
-        
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (
+                    board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]
+                ):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
 
+        return True
