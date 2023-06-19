a3b8ad8f617e507d1f9693cf71f0b0fb3d20b895
Pranay <111632387+Git4pranay@users.noreply.github.com>
2023-06-20 00:14:11 +0530

    Time: 222 ms (70.22%), Space: 28 MB (8.65%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,26 +1,26 @@
 class Solution:
     def rotate(self, nums, k):
-        # l=len(nums)
-        # z = nums[-(abs(k)%l):]+nums[0:(abs(l-(k%l)))]
-        # print(z)
-        # for i in range(l):
-        #     nums[i]=z[i]
+        l=len(nums)
+        z = nums[-(abs(k)%l):]+nums[0:(abs(l-(k%l)))]
+        print(z)
+        for i in range(l):
+            nums[i]=z[i]
         
-        k = k % len(nums)
-        l, r = 0, len(nums) - 1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l, r = l + 1, r - 1
+        # k = k % len(nums)
+        # l, r = 0, len(nums) - 1
+        # while l < r:
+        #     nums[l], nums[r] = nums[r], nums[l]
+        #     l, r = l + 1, r - 1
             
-        l, r = 0, k - 1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l, r = l + 1, r - 1
+        # l, r = 0, k - 1
+        # while l < r:
+        #     nums[l], nums[r] = nums[r], nums[l]
+        #     l, r = l + 1, r - 1
             
-        l, r = k, len(nums) - 1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l, r = l + 1, r - 1
+        # l, r = k, len(nums) - 1
+        # while l < r:
+        #     nums[l], nums[r] = nums[r], nums[l]
+        #     l, r = l + 1, r - 1
 
 
         
