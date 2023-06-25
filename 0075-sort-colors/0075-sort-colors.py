143a40d758b01cfa2a1a7aff24b17c78e509b4f3
Pranay <111632387+Git4pranay@users.noreply.github.com>
2023-06-26 00:43:20 +0530

    Time: 51 ms (49.63%), Space: 16.4 MB (30.67%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortColors(self, nums):
+        zeroc=0
+        ones=0
+        twos=0
+        for i in nums:
+            if i==0:
+                zeroc+=1
+            elif i==1:
+                ones+=1
+            else:
+                twos+=1
+        
+        ans=[0]*zeroc+[1]*ones+[2]*twos
+        for i in range(len(nums)):
+            nums[i]=ans[i]
+        
+        
