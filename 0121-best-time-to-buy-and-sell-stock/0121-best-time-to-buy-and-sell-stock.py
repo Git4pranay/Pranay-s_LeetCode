bc097365ecbd92e786d729980586f205d63c038b
Pranay <111632387+Git4pranay@users.noreply.github.com>
2023-07-07 01:13:53 +0530

    Time: 881 ms (99.16%), Space: 27.3 MB (66.57%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,21 +1,18 @@
 class Solution:
     def maxProfit(self, prices):
-        mi=999999
-        ma=0
-        ans=0
-        i=0;j=len(prices)-1
-        n1=[]
+        
+        c=prices[0]
+        d=0
         for i in prices:
-            if i<mi:
-                mi=i
-            n1.append(mi)
-        print(n1)
-        for i in range(j,-1,-1):
-             if ma<prices[i]:
-                 ma=prices[i]
-                 ans=max(ans,ma-n1[i])
+            if i<c:
+                c=i
+            if i-c > d:
+                d=i-c
+
             
-        return ans
+
+            
+        return d
             
 
 
