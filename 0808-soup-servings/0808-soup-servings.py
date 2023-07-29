fa2abac0ee41f4271c3f151703b4b1e0bdabe467
Pranay <111632387+Git4pranay@users.noreply.github.com>
2023-07-29 19:21:31 +0530

    Time: 39 ms (99.53%), Space: 17.2 MB (51.42%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.py
+++ 0808-soup-servings/0808-soup-servings.py
@@ -10,4 +10,5 @@ class Solution:
                 return 0
             else:
                 return 0.25 * (dfs(a-100,b)+dfs(a-75,b-25)+dfs(a-50,b-50)+dfs(a-25,b-75))
+            
         return 1 if n > 4450 else dfs(n,n) 
