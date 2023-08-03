46eb281bcba5ec97da0487323e15ef75787fa86e
Pranay <111632387+Git4pranay@users.noreply.github.com>
2023-08-03 19:35:15 +0530

    Time: 49 ms (37.50%), Space: 16.1 MB (98.42%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def letterCombinations(self, digits):
+        dic = {}
+        dic['2']='abc'
+        dic['3']='def'
+        dic['4']='ghi'
+        dic['5']='jkl'
+        dic['6']='mno'
+        dic['7']='pqrs'
+        dic['8']='tuv'
+        dic['9']='wxyz'
+        return list(map("".join,product(*(dic[i] for i in digits)))) if digits else []        
+
+                
+                
+            
+        
