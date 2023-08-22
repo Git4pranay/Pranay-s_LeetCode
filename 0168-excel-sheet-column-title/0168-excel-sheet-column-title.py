c0daae818d9746cb03743403a6fa93578f6a233e
Pranay <111632387+Git4pranay@users.noreply.github.com>
2023-08-22 11:23:23 +0530

    Time: 32 ms (93.98%), Space: 16.3 MB (23.11%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def convertToTitle(self, c: int) -> str:
+        l = 'abcdefghijklmnopqrstuvwxyz '
+        b = c%26
+        a = c//26
+        ans=''
+        if b==0:
+            ans+='z'
+            a=a-1
+        print(a,b)
+        ans += l[b-1]
+        if a>26:
+            while(a>26):
+                b = a%26
+                a = a//26
+                ans+=l[b-1]
+        ans+=l[a-1]
+        return ans[::-1].upper().replace(' ','')
+
+        
+        
